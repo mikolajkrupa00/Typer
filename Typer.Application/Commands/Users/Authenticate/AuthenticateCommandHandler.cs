9a08409db38828b8e83b7fc01bd5c555d7705a0f
Mikolaj Krupa <mikolajkrupa00@o2.pl>
2025-02-01 15:53:42 +0100

    added logging

--- Typer.Application/Commands/Users/Authenticate/AuthenticateCommandHandler.cs
+++ Typer.Application/Commands/Users/Authenticate/AuthenticateCommandHandler.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
 using Typer.Application.Services.JwtGenerator;
 using Typer.Application.Services.PasswordHasher;
 using Typer.Domain.Exceptions;
 using Typer.Domain.Interfaces.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace Typer.Application.Commands.Users.Authenticate
 {
@@ -16,22 +18,29 @@ namespace Typer.Application.Commands.Users.Authenticate
         private readonly IUserRepository _userRepository;
         private readonly IJwtGenerator _jwtGenerator;
         private readonly IPasswordHasher _passwordHasher;
+        readonly ILogger<AuthenticateCommandHandler> _log;
 
-        public AuthenticateCommandHandler(IUserRepository userRepository, IJwtGenerator jwtGenerator, IPasswordHasher passwordHasher)
+        public AuthenticateCommandHandler(IUserRepository userRepository, IJwtGenerator jwtGenerator, IPasswordHasher passwordHasher, ILogger<AuthenticateCommandHandler> log)
         {
             _userRepository = userRepository;
             _jwtGenerator = jwtGenerator;
             _passwordHasher = passwordHasher;
+            _log = log;
         }
 
         public async Task<UserDto> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
         {
             var user = await _userRepository.GetUserByUsername(request.Username);
-            if(user==null) throw new TyperUnauthorizedException("niepoprawne dane");
+            if(user==null)
+            {
+                _log.LogInformation($"user has tried to login using not existing username {request.Username}");
+                throw new TyperUnauthorizedException("niepoprawne dane");
+            }
             var hashedPassword = _passwordHasher.GenerateHash(request.Password, user.Salt);
             var token = _jwtGenerator.Generate(user.UserId, user.Role);
             if (_passwordHasher.GenerateHash(request.Password, user.Salt) == user.Password)
                 return new UserDto(user.Username, user.Role, user.UserId, token);
+            _log.LogInformation($"user {request.Username} tried to login using invalid password");
             throw new TyperUnauthorizedException("niepoprawne dane");
         }
     }
--- Typer.Application/Commands/Users/CreateUser/CreateUserCommandHandler.cs
+++ Typer.Application/Commands/Users/CreateUser/CreateUserCommandHandler.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,27 +20,48 @@ namespace Typer.Application.Commands.Users.CreateUser
         private readonly IUserRepository _userRepository;
         private readonly IPasswordHasher _passwordHasher;
         private readonly IJwtGenerator _jwtGenerator;
+        readonly ILogger<CreateUserCommandHandler> _log;
 
-        public CreateUserCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher, IJwtGenerator jwtGenerator)
+        public CreateUserCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher, IJwtGenerator jwtGenerator, ILogger<CreateUserCommandHandler> log)
         {
             _userRepository = userRepository;
             _passwordHasher = passwordHasher;
             _jwtGenerator = jwtGenerator;
+            _log = log;
         }
 
         public async Task<UserDto> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
-            var salt = _passwordHasher.GenerateSalt();
-            var hashedPassword = _passwordHasher.GenerateHash(request.Password, salt);
-
-            var user = await _userRepository.GetUserByUsername(request.Username);
-            if (user != null) throw new TyperBadRequestException("Login jest zajęty");
-            user = await _userRepository.GetUserByEmail(request.Email);
-            if (user != null) throw new TyperBadRequestException("Email jest zajęty");
-            var newUser = Domain.Models.User.Create(request.Username, request.Email,Domain.Enums.Roles.User, hashedPassword, salt);
-            await _userRepository.CreateAsync(newUser);
-            var token = _jwtGenerator.Generate(newUser.UserId, newUser.Role);
-            return new UserDto(token, newUser.UserId);
+            try
+            {
+                _log.LogDebug($"Getting seasons!");
+                var salt = _passwordHasher.GenerateSalt();
+                _log.LogDebug($"Hashing password!");
+                var hashedPassword = _passwordHasher.GenerateHash(request.Password, salt);
+                var user = await _userRepository.GetUserByUsername(request.Username);
+                if (user != null)
+                {
+                    _log.LogWarning($"User has omitted UI validation !");
+                    throw new TyperBadRequestException("Login jest zajęty");
+                };
+                user = await _userRepository.GetUserByEmail(request.Email);
+                if (user != null) throw new TyperBadRequestException("Email jest zajęty");
+                var newUser = Domain.Models.User.Create(request.Username, request.Email, Domain.Enums.Roles.User, hashedPassword, salt);
+                await _userRepository.CreateAsync(newUser);
+                var token = _jwtGenerator.Generate(newUser.UserId, newUser.Role);
+                _log.LogInformation($"user {request.Username} has been created");
+                return new UserDto(token, newUser.UserId);
+            }
+            catch (MySqlException e) when (request.Username.Contains("' OR 1=1; --"))
+            {
+                _log.LogCritical($"SQL Injection detected!");
+                throw;
+            }
+            catch (Exception e)
+            {
+                _log.LogError(e, $"Failed to create user {request.Username}");
+                throw;
+            }
         }
     }
 }
